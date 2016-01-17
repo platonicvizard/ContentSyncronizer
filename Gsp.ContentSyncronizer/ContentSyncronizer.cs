ffd9b8cefdc71cb3a300f7d2d2d94ade84c5bfcc
Lisandro Martinez <lmartinezm@msn.com>
2016-01-17 14:01:55 -0500

    started settings

---
+++ Gsp.ContentSyncronizer/ContentSyncronizer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Gsp.ContentSyncronizer
+{
+    public partial class ContentSyncronizer : ServiceBase
+    {
+        public ContentSyncronizer()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowMessageBox("TEST");
+        }
+         public void LoadSettings()
+        {
+            var reader = new XmlTextReader("settings.xml");
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        Console.Write("<" + reader.Name);
+
+                        while (reader.MoveToNextAttribute()) // Read the attributes.
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+        }
+       public void ShowMessageBox(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ Gsp.ContentSyncronizer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gsp.ContentSyncronizer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+#if (!DEBUG)
+           ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ContentSyncronizer()
+            };
+            ServiceBase.Run(ServicesToRun);
+#else
+            ContentSyncronizer myServ = new ContentSyncronizer();
+            myServ.ShowMessageBox("Testing 123");
+            // here Process is my Service function
+            // that will run when my service onstart is call
+            // you need to call your own method or function name here instead of Process();
+#endif
+        }
+    }
+}
